dd2a51a45a58f704021e608732bede0981592c9f
CzzzzH <morphling233@gmail.com>
2021-04-18 04:50:59 +0800

    Add the choice screen

--- src/SnakePlayer.cpp
+++ src/SnakePlayer.cpp
@@ -234,6 +234,3 @@ void SnakePlayer::Shine_Food()
 	dm->setDot(food.pos.x, food.pos.y, food.pos.z, twinkle);
 	twinkle = !twinkle;
 }
-
-// 版权声明：本文为CSDN博主「ZhnJa」的原创文章，遵循CC 4.0 BY-SA版权协议，转载请附上原文出处链接及本声明。
-// 原文链接：https://blog.csdn.net/czhnja/article/details/16891621
--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,3 @@
-/*
- *  A3D8_Controller_Advanced.ino
- * 
- *	animation with the support of dot-matrix library
- *	Advanced animation sample on arduino managing 3D8S 8x8x8 led cubic display
- *	by connecting the serial port on 3D8S board (P2)
- *
- *	P2.1 -> VCC (5V)
- *	P2.2 -> #1 (TX)
- *	P2.3 -> #0 (RX)
- *	P2.4 -> GND (0V)
- *
- *	Disconnection would be needed when upload the sketch to arduino.
- *
- *  Created on: 2012-7-3
- *	Author: Weihong Guan
- *	Blog: http://aguegu.net
- *	E-mail: weihong.guan@gmail.com
- *
- *	Code license: Attribution-NonCommercial-ShareAlike 3.0 Unported (CC BY-NC-SA 3.0)
- *	http://creativecommons.org/licenses/by-nc-sa/3.0/
- *	source host: https://github.com/aguegu/dot-matrix
- *
- */
-
 #include <Arduino.h>
 #include "Controller_A3D8.h"
 #include "JY61.h"
@@ -486,6 +461,7 @@ void loop()
                 showResult(game->win);
                 gameStart = false;
             }
+            if (gameChoice == 0) delay(50);
         }
         else
         {
@@ -509,10 +485,10 @@ void loop()
             if (diff > 180) diff -= 360;
             else if (diff < -180) diff += 360;
 
-            if (abs(diff) > 60)
+            if (abs(diff) > 70)
             {   
                 if (gameChoice == 0) game = new Dodger();
-                else game = new Dodger();
+                else game = new SnakePlayer();
                 game->init(initBuf, &dm);
                 gameStart = true;
                 return;
@@ -541,9 +517,9 @@ void loop()
             dm.setDot(2, 5, 7);
             dm.setDot(3, 6, 7);
             dm.setDot(4, 6, 7);
+            delay(50);
         }
         cube.putDM();
-        delay(50);
     }
     // Cube Program End
 }
