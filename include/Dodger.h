f47575de942bf0f000954042266a3b1d1d9d21f7
CzzzzH <morphling233@gmail.com>
2021-04-18 04:36:07 +0800

    Finish the Dogger

--- include/Dodger.h
+++ include/Dodger.h
@@ -6,14 +6,17 @@
 class Dodger: public Game
 {
 public:
-    uint8_t index[3], moveSign[3], rainState[112];
+    uint8_t index[3], rainState[112];
+    int moveSign[3];
     int initMove[3], activeSequence[112];
+    int life, last;
     float position[3], rainPosition[112][3];
     bool playerOn;
     void init(uint8_t* buf, DotMatrix3D* dm);
     void update(uint8_t* buf);
     void movePlayer(uint8_t* buf);
     void moveRain();
+    void updateLED(int initX, int initZ, int num);
 };
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -23,11 +23,11 @@
  *
  */
 
+#include <Arduino.h>
 #include "Controller_A3D8.h"
 #include "JY61.h"
 #include "Zigbee.h"
 #include "SnakePlayer.h"
-#include <Arduino.h>
 #include "Dodger.h"
 
 #define BYTE_LENGTH 64
@@ -38,8 +38,17 @@ DotMatrix3D dm(1);
 Controller_A3D8 cube(dm, Serial1);
 Game* game;
 byte * cache;
-uint8_t initBuf[8];
+uint8_t initBuf[8], gameChoice = 0;
 bool gameStart = false;
+
+const uint8_t PROGMEM PATTERN_LOVE[] =
+{ 0x00, 0x81, 0x81, 0xFF, // I
+		0x38, 0xFC, 0xFE, 0x3F, //heart
+		0x00, 0xFF, 0xFF, 0x01, // U
+		};
+
+const uint8_t PROGMEM PATTERN_ARROW[] =
+{ 0x08, 0x14, 0x22, 0x77, 0x14, 0x14, 0x14, 0x14, 0x14, 0x1c, };
 
 void setup()
 {   
@@ -50,18 +59,300 @@ void setup()
     // Player's Controller Program End
 
     // Cube Program Begin
-	  cache = new byte(BYTE_LENGTH);
+    cache = new byte(BYTE_LENGTH);
     Serial.begin(9600);
     Serial1.begin(57600);
+    Serial3.begin(115200);
     cube.sendBrightness(255);
     cube.sendMode(Controller_A3D8_Basic::OLD);
     dm.clear();
     cube.putDM();
     randomSeed(analogRead(0));
     while (!Zigbee::getBuffer(initBuf)) {}
-    game = new Dodger();
-    game->init(initBuf, &dm);
     // Cube Program End
+}
+
+void animationFlash(word k)
+{
+	byte value = k & 0x01 ? 0xff : 0x00;
+	cube.sendGlobal(value);
+}
+
+void animationBreathe(word k)
+{
+	k = k % 0x200;
+	byte brightness = k < 0x100 ? 0xff - k : k - 0xff;
+
+	cube.sendGlobal(0xff);
+	cube.sendBrightness(brightness);
+}
+
+void animationFacetScan(word k)
+{
+	byte value = _BV(k%8);
+	cube.sendGlobal(value);
+}
+
+void animationBlockScan(word k)
+{
+	k = k % 8;
+	byte value = _BV(k+1) - 1;
+	cube.sendGlobal(value);
+}
+
+void animationFlowZPosi(word k)
+{
+	dm.setMoveDirection(DotMatrix3D::Z_POSI);
+	dm.move(false);
+	for (byte i = 0; i < random(4); i++)
+	{
+		byte x = random(8);
+		byte y = random(8);
+		dm.setDot(x, y, 0);
+	}
+	cube.putDM();
+}
+
+void animationMoveSide(word k)
+{
+	k %= 144;
+	if (k == 0 || k == 72)
+		dm.clear(0x01);
+
+	byte index;
+
+	for (word i = dm.countBytes(); i;)
+	{
+		i--;
+		byte temp = dm.getByte(i) & 0x81;
+		dm.setByte(i, ((dm.getByte(i) & 0x7e) << 1) | temp);
+	}
+
+	if (bitRead(dm.orValue(),0) == true)
+	{
+		do
+		{
+			index = random(64);
+		} while (dm.getByte(index) != 0x01);
+		dm.setByte(index, 0x02);
+	}
+
+	k < 72 ? cube.putDM() : cube.putDMrevZ();
+
+}
+
+void animationMoveSideQuick(word k)
+{
+	k %= 16;
+
+	if (k % 8 == 0)
+	{
+		for (byte i = 0; i < BYTE_LENGTH; i++)
+		{
+			cache[i] = _BV(random(8));
+		}
+	}
+
+	if (k < 8)
+		for (byte i = 0; i < BYTE_LENGTH; i++)
+		{
+			byte temp = dm.getByte(i);
+			if (dm.getByte(i) < cache[i])
+				dm.setByte(i, temp << 1);
+		}
+	else
+	{
+		for (byte i = 0; i < BYTE_LENGTH; i++)
+		{
+			byte temp = dm.getByte(i);
+			if (dm.getByte(i) < 0x80)
+				dm.setByte(i, temp << 1);
+		}
+	}
+
+	cube.putDM();
+}
+
+void animationWave2D(word k)
+{
+	k %= 16;
+	byte value = k < 8 ? k : 15 - k;
+
+	dm.setMoveDirection(DotMatrix3D::Y_POSI);
+	dm.move(false);
+
+	dm.setByte(value, 0xff);
+	cube.putDM();
+}
+
+void animationWave3D(word k)
+{
+	k %= 16;
+	byte value = k < 8 ? k : 15 - k;
+
+	for (byte i = 0; i < 8; i++)
+		cache[i] = dm.getByte(i);
+
+	dm.setMoveDirection(DotMatrix3D::Y_POSI);
+	dm.move(false);
+
+	for (byte i = 0; i < 8; i++)
+		dm.setByte(i, cache[i]);
+
+	for (byte i = 7; i; i--)
+		dm.setByte(i, dm.getByte(i - 1));
+
+	dm.setByte(0x00, _BV(value));
+
+	cube.putDM();
+}
+
+void animationWaveShake(word k)
+{
+	k %= 16;
+	byte value = k < 8 ? k : 15 - k;
+
+	dm.setMoveDirection(DotMatrix3D::X_POSI);
+	dm.move(false);
+	dm.setLine(0, value, 7, 7 - value);
+
+	cube.putDM();
+}
+
+void animationWaveRotate(word k)
+{
+	k %= 28;
+	dm.setMoveDirection(DotMatrix3D::X_POSI);
+	dm.move(false);
+
+	byte temp = k % 7;
+	cache[0] = 0;
+	cache[1] = temp;
+	cache[2] = 7;
+	cache[3] = 7 - temp;
+
+	byte i = k / 7;
+
+	dm.setLine(cache[i % 4], cache[(i + 1) % 4], cache[(i + 2) % 4],
+			cache[(i + 3) % 4]);
+
+	cube.putDM();
+}
+
+void animationDance(word k)
+{
+	if (k == 0)
+	{
+		cache[0] = 0;
+		cache[1] = 0;
+	}
+
+	char x = cache[0];
+	char y = cache[1];
+
+	dm.setMoveDirection(DotMatrix3D::Z_POSI);
+	dm.move();
+
+	char vx = random(3) - 1;
+	char vy = random(3) - 1;
+
+	x += vx;
+	y += vy;
+
+	if (x >= 8 || x < 0)
+	{
+		x -= vx + vx;
+	}
+
+	if (y >= 8 || y < 0)
+	{
+		y -= vy + vy;
+	}
+
+	cache[0] = x;
+	cache[1] = y;
+
+	dm.setDot(x, y, 0);
+	cube.putDM();
+}
+
+void animationRotateArrow(word k)
+{
+	k %= 28;
+	dm.rotate(0, false, false);
+	if (k < 10)
+		dm.setByte(0, pgm_read_byte_near(PATTERN_ARROW+k));
+	cube.putDM();
+
+}
+
+void animationRotateLove(word k)
+{
+	k %= 28 * 6;
+
+	if (k % 56 == 0)
+	{
+		for (byte i = 0; i < 4; i++)
+		{
+			byte value = DotMatrix::reverseByte(
+					pgm_read_byte_near(PATTERN_LOVE+(k/56)*4+i));
+			dm.setByte(0x20 + i, value);
+			dm.setByte(0x27 - i, value);
+			dm.setByte(0x18 + i, value);
+			dm.setByte(0x1f - i, value);
+		}
+	}
+
+	dm.rotateSync(true, true);
+
+	cube.putDM();
+}
+
+void animationOneByOne(word k)
+{
+	if (k % 512 == 0)	dm.clear(0x00);
+
+	byte col, row;
+
+	if (dm.andValue() != 0xff)
+	{
+		do
+		{
+			col = random(64);
+			row = random(8);
+		} while (dm.getDot(col, row) == true);
+		dm.DotMatrix::setDot(col, row, true);
+	}
+
+	k /= 512;
+	if (k & 0x01)
+		cube.putReverseDM();
+	else
+		cube.putDM();
+}
+
+void callAnimation(void (*p)(word), word span, word times, byte init_value,
+		Controller_A3D8_Basic::InputMode mode)
+{
+	static word frame_id = 0;
+
+	cube.sendMode(mode);
+	dm.clear(init_value);
+
+	while (times--)
+	{
+		(*p)(frame_id++);
+		delay(span);
+	}
+	frame_id = 0;
+}
+
+void callAnimationInModes(void (*p)(word), word span, word times,
+		byte init_value)
+{
+	for (byte i = 0; i < 3; i++)
+		callAnimation(p, span, times, init_value,
+				(Controller_A3D8_Basic::InputMode) i);
 }
 
 // 测试背景白灯模式
@@ -122,6 +413,46 @@ void testAll()
             }
 }
 
+void showResult(int win)
+{   
+    if (win == 2)
+    {
+        for (int i = 0; i < 8; ++i)
+        {
+            dm.setDot(i, 7, i);
+            dm.setDot(i, 7, 7 - i);
+        }
+    }
+    else
+    {
+        callAnimation(animationFlash, 0xF0, 0x08, 0x00, Controller_A3D8_Basic::XYZ);
+        callAnimation(animationBreathe, 0x08, 0xff * 4, 0xff, Controller_A3D8_Basic::XYZ);
+        callAnimationInModes(animationFacetScan, 0x40, 0x08, 0x00);
+        callAnimationInModes(animationBlockScan, 0x40, 0x08, 0x00);
+        callAnimationInModes(animationFlowZPosi, 0x40, 0x40, 0x00);
+
+        callAnimationInModes(animationMoveSide, 0x30, 144, 0x00);
+        callAnimationInModes(animationMoveSideQuick, 0x40, 0x10, 0x01);
+
+        callAnimationInModes(animationWave2D, 0x40, 14 * 4, 0x00);
+        callAnimation(animationWave3D, 0x40, 14 * 8, 0x00, Controller_A3D8_Basic::ZXY);
+        callAnimation(animationWaveShake, 0x40, 14 * 8, 0x00, Controller_A3D8_Basic::XYZ);
+
+        callAnimation(animationWaveRotate, 0x20, 14 * 8, 0x00, Controller_A3D8_Basic::XYZ);
+        callAnimation(animationWaveRotate, 0x20, 14 * 8, 0x00, Controller_A3D8_Basic::YZX);
+
+        callAnimation(animationDance, 0x20, 0x80, 0x00, Controller_A3D8_Basic::ZXY);
+
+        callAnimation(animationRotateArrow, 0x20, 28 * 3, 0x00, Controller_A3D8_Basic::YZX);
+        callAnimation(animationRotateArrow, 0x30, 28 * 3, 0x00, Controller_A3D8_Basic::ZXY);
+
+        callAnimation(animationRotateLove, 0x30, 28 * 6, 0x00, Controller_A3D8_Basic::ZXY);
+
+        callAnimation(animationOneByOne, 0x10, 0x400, 0x00, Controller_A3D8_Basic::XYZ);
+    }
+    
+}
+
 void loop()
 {   
     // Player's Controller Program Begin
@@ -132,6 +463,9 @@ void loop()
     // buf[4] = (unsigned char)((int)(abs(JY61::Angle[1])));
     // buf[6] = (unsigned char)((int)(abs(JY61::Angle[2])));
     // buf[7] = 234;
+    // // Serial.println(buf[2]);
+    // // Serial.println(buf[4]);
+    // // Serial.println(buf[6]);
     // JY61::Angle[0] >= 0 ? buf[1] = 0 : buf[1] = 1;
     // JY61::Angle[1] >= 0 ? buf[3] = 0 : buf[3] = 1;
     // JY61::Angle[2] >= 0 ? buf[5] = 0 : buf[5] = 1;
@@ -144,9 +478,71 @@ void loop()
     unsigned char buf[8];
     if (Zigbee::getBuffer(buf))
     {   
-        dm.clear();
         if (gameStart)
+        {
             game->update(buf);
+            if (game->win != 0)
+            {
+                showResult(game->win);
+                gameStart = false;
+            }
+        }
+        else
+        {
+            dm.clear();
+            int zPos, diff, currentMove, initMove;
+            initBuf[3] > 0 ? initMove = -int(initBuf[4]) : initMove = int(initBuf[4]);
+            buf[3] > 0 ? currentMove = -int(buf[4]) : currentMove = int(buf[4]);
+            diff = currentMove - initMove;
+            if (diff > 180) diff -= 360;
+            else if (diff < -180) diff += 360;
+
+            if (abs(diff) > 30)
+            {   
+                if (diff < 0) gameChoice = 1;
+                else gameChoice = 0;
+            }
+
+            initBuf[5] > 0 ? initMove = -int(initBuf[6]) : initMove = int(initBuf[6]);
+            buf[5] > 0 ? currentMove = -int(buf[6]) : currentMove = int(buf[6]);
+            diff = currentMove - initMove;
+            if (diff > 180) diff -= 360;
+            else if (diff < -180) diff += 360;
+
+            if (abs(diff) > 60)
+            {   
+                if (gameChoice == 0) game = new Dodger();
+                else game = new Dodger();
+                game->init(initBuf, &dm);
+                gameStart = true;
+                return;
+            }
+
+            if (gameChoice == 0) zPos = 0;
+            else zPos = 7;
+            for (int i = 0; i < 7; ++i)
+            {
+                dm.setDot(i, 0, zPos);
+                dm.setDot(i, 7, zPos);
+                dm.setDot(0, i, zPos);
+                dm.setDot(7, i, zPos);
+            }
+            // 1
+            for (int i = 2; i < 6; ++i)
+                dm.setDot(4, i, 0);
+            dm.setDot(3, 4, 0);
+            // 2
+            for (int i = 2; i < 6; ++i)
+                dm.setDot(i, 1, 7);
+            dm.setDot(2, 2, 7);
+            dm.setDot(3, 3, 7);
+            dm.setDot(4, 4, 7);
+            dm.setDot(5, 5, 7);
+            dm.setDot(2, 5, 7);
+            dm.setDot(3, 6, 7);
+            dm.setDot(4, 6, 7);
+        }
+        
         cube.putDM();
         delay(50);
     }
